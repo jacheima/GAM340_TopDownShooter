0e18b80e9c9ade5bd155a141449b8baa1bbd6930
Jacquelynne <jacheima@uat.edu>
2020-01-14 17:23:32 -0700

    Got the animations set up

---
+++ GPE340/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField, Tooltip("The max speed of the player")]
+    private float speed = 10f;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        
+        
+    }
+
+    public void Move(Vector2 direction)
+    {
+        animator.SetFloat("Horizontal", direction.x * speed);
+        animator.SetFloat("Vertical", direction.y * speed);
+    }
+
+}
---
+++ GPE340/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       player.Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+    }
+}
